047095f02a363da909c012209333a7c877a61de2
DESKTOP-VPEEPF9\ilici <drobnjak84@gmail.com>
2022-10-17 10:37:59 +0200

    My Homework

---
+++ VEZBE/17. ARRAYS/ArrayMilosDrobnjak.js
@@ -0,0 +1,33 @@
+// 1.Write an array of months in a year.
+// Using console.log display June, October and January from the array.
+var months_in_a_year = ["January", "February", "March", "April", "May", "June", "July", "August",
+ "September", "October", "November", "December"] 
+     
+ console.log(months_in_a_year[5],months_in_a_year[9],months_in_a_year[0]);
+     
+//2.Write an arrays of days in a week. Using console.log display Sunday from the array. 
+var days_in_a_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+   console.log(days_in_a_week[6])
+   
+//3.Print all negative elements from the array [2, -4, 5, -2, -11].
+var elements =[2, -4, 5, -2, -11]
+    console.log(elements[1],elements[3],elements[4])
+
+//4.Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18, 72, 14, 9].    
+var elements1 = [5, 15, -5, 20, 12, 18, 72, 14, 9]
+    console.log(elements1[1],elements1[4],elements1[5],elements1[6],elements1[8])
+    
+//5.What is the index of number 24 in the following array?
+// [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+// Using console.log:
+// ●	Display the 3rd element of the array,
+// ●	Display the 2nd element of the 3rd element.
+var elemen = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]
+    console.log(elemen[0][3]);
+
+    console.log(elemen[2]);
+    
+    console.log(elemen[2][1])
+
+    
