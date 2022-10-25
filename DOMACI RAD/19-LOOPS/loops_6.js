f1158a754c8423a44326ac1ab9c5ef0f3827b8ee
goranc89 <114668056+goranc89@users.noreply.github.com>
2022-10-25 20:51:32 +0200

    Update loops_6.js

--- DOMACI RAD/19-LOOPS/loops_6.js
+++ DOMACI RAD/19-LOOPS/loops_6.js
@@ -8,7 +8,11 @@ for (var i in x) {
 console.log(x.length)
 
 
-
+///////////////////////////
+// Ovde je cilj ispisati koliko je slova u reci Eclip542se. Primetices da je to 7 slova i 3 broja, a trebao si da ispises koliko slova ima :)
+// Ovaj zadatak uradi ponovo na nacin da ti se prikaze koliko slova ima u zadatoj reci.
+// Trenutno samo ispisujes duzinu reci pocetne reci, a to je 10, i to nije tacno resenje.
+// Ako treba pomoc ili konsultacija - slobodno mi pisi
 
 
 
